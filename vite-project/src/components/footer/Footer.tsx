7f64afe0af262d9ca1d3ebd0fc8fc3a654d853f1
Mason Josefchuk <mjosefchuk@rrc.ca>
2026-01-16 09:16:36 -0600

    Changed Footer tsx to force new additions to follow an interface and created an Array for easier mamangement of group members and links.

--- vite-project/src/components/footer/Footer.tsx
+++ vite-project/src/components/footer/Footer.tsx
@@ -1,5 +1,16 @@
 import './Footer.css';
 
+interface NameOfGroupMembers {
+  memberName: string
+  gitHubLink: string
+};
+
+const groupMembers: NameOfGroupMembers[] = [
+  {memberName: "Mason Josefchuk", gitHubLink: "https://github.com/Ipadman321"},
+  {memberName: "Peter Firlotte", gitHubLink: "https://github.com/pfirlotte-rrc"},
+  {memberName: "Cj Gonzales", gitHubLink: "https://github.com/cgonzales2-rrc"}
+];
+
 export function Footer () {
     const currentYear = new Date().getFullYear();
 
@@ -7,22 +18,11 @@ export function Footer () {
     <footer  className="Footer">
       <div className="Footer-Content">
         <p className="Copyright-Text">&copy; {currentYear} Works on My Computer LLC. All rights reserved.</p>
-        <div className="Footer-Project-Link">
-          <a href="https://github.com/pfirlotte-rrc/COMP-4002-Project" target="_blank">
+          <a className="Footer-Project-Link" href="https://github.com/pfirlotte-rrc/COMP-4002-Project" target="_blank">
           <h2>GitHub Project</h2>
           </a>
-        </div>
         <div className="Footer-Links">
           <h3>Group Members</h3>
-          <a href="https://github.com/pfirlotte-rrc" target="_blank">
-          <h4>Peter Firlotte</h4>
-          </a>
-          <a href="https://github.com/cgonzales2-rrc" target="_blank"> 
-          <h4>Cj Gonzales</h4>
-          </a>
-          <a href="https://github.com/Ipadman321" target="_blank"> 
-          <h4>Mason Josefchuk</h4>
-          </a>
         </div>
       </div>
     </footer>
