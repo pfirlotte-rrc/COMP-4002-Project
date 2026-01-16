53fdd1787057809c2c5594ecc6806832331f0ddc
Mason Josefchuk <mjosefchuk@rrc.ca>
2026-01-16 11:14:40 -0600

    Reformatted the way the code was structured for ease of use and changed the order that the html is rendered in.

--- vite-project/src/components/footer/Footer.tsx
+++ vite-project/src/components/footer/Footer.tsx
@@ -1,14 +1,14 @@
 import './Footer.css';
 
 interface NameOfGroupMembers {
-  memberName: string
-  gitHubLink: string
+  memberName: string,
+  githubLink: string
 };
 
 const groupMembers: NameOfGroupMembers[] = [
-  {memberName: "Mason Josefchuk", gitHubLink: "https://github.com/Ipadman321"},
-  {memberName: "Peter Firlotte", gitHubLink: "https://github.com/pfirlotte-rrc"},
-  {memberName: "Cj Gonzales", gitHubLink: "https://github.com/cgonzales2-rrc"}
+  {memberName: "Mason Josefchuk", githubLink: "https://github.com/Ipadman321"},
+  {memberName: "Peter Firlotte", githubLink: "https://github.com/pfirlotte-rrc"},
+  {memberName: "Cj Gonzales", githubLink: "https://github.com/cgonzales2-rrc"}
 ];
 
 export function Footer () {
@@ -16,22 +16,22 @@ export function Footer () {
 
   return (
     <footer className="Footer">
-        <p className="Copyright-Text">&copy; {currentYear} Works on My Computer LLC. All rights reserved.</p>
-          <a className="Footer-Project-Link" href="https://github.com/pfirlotte-rrc/COMP-4002-Project" target="_blank">
-          <h2>GitHub Project</h2>
-          </a>
-          <h3 className="Group-Member-Tag">Group Members</h3>
-          <a className="Group-Member-List">
-            <ul>
-              {groupMembers.map((member, index) => (
-                <li key={index} className="Group-Member-Item">
-                  <a href={member.gitHubLink} target="_blank">
-                  {member.memberName}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </a>
+      <a className="Footer-Project-Link" href="https://github.com/pfirlotte-rrc/COMP-4002-Project" target="_blank">
+        <h2>GitHub Project</h2>
+      </a>
+      <p className="Copyright-Text">&copy; {currentYear} Works on My Computer LLC. All rights reserved.</p>
+      <h3 className="Group-Member-Tag">Group Members</h3>
+      <a className="Group-Member-List">
+        <ul>
+          {groupMembers.map((member, index) => (
+            <li key={index} className="Group-Member">
+              <a href={member.githubLink} target="_blank">
+              {member.memberName}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </a>
     </footer>
   );
 };
