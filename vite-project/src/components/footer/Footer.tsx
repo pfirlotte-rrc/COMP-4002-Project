fed9734954cade2019d92c10bd7e2418cbe284ba
Mason Josefchuk <mjosefchuk@rrc.ca>
2026-01-15 21:59:02 -0600

    Enhanced GitHub Project hyperlink to create new page when clicked. Seperated sections of Footer for CSS styling.

--- vite-project/src/components/footer/Footer.tsx
+++ vite-project/src/components/footer/Footer.tsx
@@ -1,14 +1,20 @@
+import './Footer.css';
+
 export function Footer () {
     const currentYear = new Date().getFullYear();
 
   return (
     <footer  className="Footer">
       <div className="Footer-Content">
-        <p>&copy; {currentYear} Works on My Computer LLC. All rights reserved.</p>
-        <div className="Footer-Links">
-          <a href="https://github.com/pfirlotte-rrc/COMP-4002-Project">
+        <div>
+          <p>&copy; {currentYear} Works on My Computer LLC. All rights reserved.</p>
+        </div>
+        <div className="Footer-Project-Link">
+          <a href="https://github.com/pfirlotte-rrc/COMP-4002-Project" target="_blank">
           <h2>GitHub Project</h2>
           </a>
+        </div>
+        <div className="Footer-Links">
           <h3>Group Members</h3>
           <a href="https://github.com/pfirlotte-rrc" target="_blank">
           <h4>Peter Firlotte</h4>
@@ -20,7 +26,7 @@ export function Footer () {
           <h4>Mason Josefchuk</h4>
           </a>
         </div>
-        </div>
+      </div>
     </footer>
   );
 };
