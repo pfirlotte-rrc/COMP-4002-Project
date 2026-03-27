fb0c10d86c4e38eeea9312c35dcdeb67983b32f6
Peter <pfirlotte@academic.rrc.ca>
2026-03-27 09:55:40 -0500

    Create seedData file

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -3,6 +3,8 @@ import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
+import { articleSeedData } from "./seedData";
+
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
@@ -13,75 +15,15 @@ const prisma = new PrismaClient({ adapter });
 // see https://www.prisma.io/docs/orm/prisma-migrate/workflows/seeding
 async function main() {
     // clear table
-    await prisma.term.deleteMany();
+    await prisma.article.deleteMany();
 
     // insert terms to db
-    const assignArticles = await prisma.post.createManyAndReturn({
-    data: {
-        name: "TypeScript Date Tutorials", 
-        newsArticle: "https://pythonguides.com/typescript-date/", 
-        publishDate: new Date(2026, 0, 27), 
-        description: "This article is a comprehensive tutorial explaining how to create, format, and manipulate dates using TypeScript's native Date object. It covers practical topics like time zones, custom formatting, and performing common operations, and also suggests libraries like date-fns for complex tasks.", 
-        views: 37,
-        ratings: {
-        create: [
-            {
-            rating: {
-                value: 2,
-                user: {
-                create: {
-                    name: "User1",
-                },
-                },
-            },
-            },
-            {
-            rating: {
-                value: 4,
-                user: {
-                create: {
-                    name: "User2",
-                },
-                },
-            },
-            },
-            {
-            rating: {
-                value: 4,
-                user: {
-                create: {
-                    name: "User3",
-                },
-                },
-            },
-            },
-            {
-            rating: {
-                value: 5,
-                user: {
-                create: {
-                    name: "User5",
-                },
-                },
-            },
-            },
-        ],
-        },
-        categories: {
-        create: [
-            {
-            category: {
-                create: [
-                {
-                    name: "Programming", "Typescript"
-                }
-                ],
-            },
-            },
-        ],
-        },
-    }
-});
+    const assignArticles = await prisma.article.createManyAndReturn(
+        {
+            data: articleSeedData,
+            skipDuplicates: true
+        }
+    );
 
     console.log(`CREATED TERMS: ${assignArticles}`);
 };
