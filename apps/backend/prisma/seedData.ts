49beb9f56f5948957808c1fed09fae1ab3ea4b8a
Peter <pfirlotte@academic.rrc.ca>
2026-03-27 10:25:43 -0500

    Correct minor issue with seedData file.

--- apps/backend/prisma/seedData.ts
+++ apps/backend/prisma/seedData.ts
@@ -6,7 +6,7 @@ export const articleSeedData = [
     newsArticle: "https://pythonguides.com/typescript-date/", 
     publishDate: new Date(2026, 0, 27), 
     description: "This article is a comprehensive tutorial explaining how to create, format, and manipulate dates using TypeScript's native Date object. It covers practical topics like time zones, custom formatting, and performing common operations, and also suggests libraries like date-fns for complex tasks.", 
-    views: 37
+    views: 37,
     ratings: {
       create: [
         {
@@ -37,9 +37,7 @@ export const articleSeedData = [
           rating: {
             value: 5,
             user: {
-              create: {
-                name: "User5",
-              },
+              create: {name: "User5"},
             },
           },
         },
