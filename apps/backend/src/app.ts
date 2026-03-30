66b2c58754fde8148724a235eef9d4fbf90cbb57
Mason Josefchuk <mjosefchuk@rrc.ca>
2026-03-30 12:08:50 -0500

    Refactored app.ts to use CORS headers.

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,10 +1,12 @@
 import express, {Express} from "express";
+import cors from "cors";
+import corsOptions from "../config/cors";
+import setupSwagger from "../config/swagger";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import ratingRoutes from "./api/v1/routes/ratingRoutes";
 import hiddenArticleRoutes from "./api/v1/routes/hiddenArticleRoutes"
-// import termRoutes from "./api/v1/routes/termRoutes";
-// import errorHandler from "./api/v1/middleware/errorHandler";
+
 
 // initialize express application
 const app: Express = express();
@@ -16,6 +18,14 @@ app.use(morgan("combined"));
 
 // allow express to parse json
 app.use(express.json());
+
+// add Cross-Origin Resource Sharing middleware
+// This will refuse requests from origins that do not fulfill corsOptions requirements
+// see https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS
+app.use(cors(corsOptions));
+
+// invoke swagger middleware for serving docs in /api-docs
+setupSwagger(app);
 
 // listen for requests on root and send simple text response
 app.get("/",  (_req, res) => {
