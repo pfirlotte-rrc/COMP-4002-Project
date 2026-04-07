be2dda30eab0e07e6870c2c92a327e4d7366480b
Peter <pfirlotte@academic.rrc.ca>
2026-04-07 12:59:37 -0500

    Create Clerk wraparound layer in front-end main.

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* the VITE_ prefix allows vite to expose .env variables  */}
+    {/* The ClerkProvider allows use of Clerk auth, similar to context, in app */}
+    <ClerkProvider publishableKey = {import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+        {/* allows for routing in the application */}
+      <BrowserRouter>
+        {/* renders the App component */}
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
