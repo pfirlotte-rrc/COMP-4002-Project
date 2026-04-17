85055b59b600447be83ff90bae7585f510edac30
Peter <pfirlotte@academic.rrc.ca>
2026-04-17 08:41:07 -0500

    Create clerk main.tsx wraparound.

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey = {import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+
   </StrictMode>,
 )
